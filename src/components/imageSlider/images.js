a2073b935cff5c0f16d8811e4ca0ead4c7b43236
Erik Erlandsson <Erik.Erlandsson@cybercom.com>
2021-10-22 13:09:35 +0200

    basic img viewing

---
+++ src/components/imageSlider/images.js
@@ -0,0 +1,75 @@
+const host = "http://localhost:3000/portfolio.github.io/";
+const location = "img/";
+
+export const images = [
+  {
+    id: 1,
+    name: "bandy1",
+    url: host + location + "bandy1.jpg",
+  },
+  {
+    id: 2,
+    name: "bandy2",
+    url: host + location + "bandy2.jpg",
+  },
+  {
+    id: 3,
+    name: "bandy3",
+    url: host + location + "bandy3.jpg",
+  },
+  {
+    id: 4,
+    name: "bandy4",
+    url: host + location + "bandy4.jpg",
+  },
+  {
+    id: 5,
+    name: "bandy5",
+    url: host + location + "bandy5.jpg",
+  },
+  {
+    id: 6,
+    name: "handboll1",
+    url: host + location + "handboll1.jpg",
+  },
+  {
+    id: 7,
+    name: "handboll2",
+    url: host + location + "handboll2.jpg",
+  },
+  {
+    id: 8,
+    name: "handboll3",
+    url: host + location + "handboll3.jpg",
+  },
+  {
+    id: 9,
+    name: "rock1",
+    url: host + location + "rock1.jpg",
+  },
+  {
+    id: 10,
+    name: "rock2",
+    url: host + location + "rock2.jpg",
+  },
+  {
+    id: 11,
+    name: "rugby1",
+    url: host + location + "rugby1.jpg",
+  },
+  {
+    id: 12,
+    name: "rugby2",
+    url: host + location + "rugby2.jpg",
+  },
+  {
+    id: 13,
+    name: "rugby3",
+    url: host + location + "rugby3.jpg",
+  },
+  {
+    id: 14,
+    name: "rugby4",
+    url: host + location + "rugby4.jpg",
+  },
+];
--- src/components/page/AboutPage.jsx
+++ src/components/page/AboutPage.jsx
@@ -1,21 +1,19 @@
-import { Container, ContainerTypes } from "../layout/Container";
+import { Container } from "../layout/Container";
 
 export default function AboutPage() {
   return (
-    <Container type={ContainerTypes.Main}>
-      <Container type={ContainerTypes.InnerCentered}>
-        <h2>About Page</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim
-          tincidunt eros. Phasellus auctor lorem in vulputate luctus. Vestibulum
-          rhoncus facilisis magna, at viverra magna feugiat vel. In sagittis
-          libero id pretium egestas. Quisque mi metus, bibendum sed lorem ut,
-          egestas porta orci. Sed interdum egestas enim varius pellentesque.
-          Duis pellentesque ut ligula et suscipit. Vestibulum ante ipsum primis
-          in faucibus orci luctus et ultrices posuere cubilia curae; Integer ac
-          neque et augue pellentesque sagittis.
-        </p>
-      </Container>
+    <Container>
+      <h2>About Page</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim
+        tincidunt eros. Phasellus auctor lorem in vulputate luctus. Vestibulum
+        rhoncus facilisis magna, at viverra magna feugiat vel. In sagittis
+        libero id pretium egestas. Quisque mi metus, bibendum sed lorem ut,
+        egestas porta orci. Sed interdum egestas enim varius pellentesque. Duis
+        pellentesque ut ligula et suscipit. Vestibulum ante ipsum primis in
+        faucibus orci luctus et ultrices posuere cubilia curae; Integer ac neque
+        et augue pellentesque sagittis.
+      </p>
     </Container>
   );
 }
